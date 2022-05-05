2799e0f018a058b6a32748d7dcb87b21ef4262c2
RodrigoMMartins1 <99209230+RodrigoMMartins1@users.noreply.github.com>
2022-05-05 13:18:26 -0300

    Add files via upload
    Exercicio da altura

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/Semana 3/exercicio1/exerci.js
@@ -0,0 +1,13 @@
+//comentário em js
+
+//criando uma mensagem de aviso
+function ex1(){
+var v0 = document.getElementById('v0').value;
+const g = 10;
+
+tSubida = v0/g;
+document.getElementById('vem').innerHTML = "TEMPO DE SUBIDA = " + tSubida;
+console.log("TEMP SUBIDA" +tSubida);
+}
+
+
