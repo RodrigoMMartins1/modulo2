ad75bdc8aa2da6bd2c4202b69ecb4cd15bcbb18c
RodrigoMMartins1 <99209230+RodrigoMMartins1@users.noreply.github.com>
2022-05-29 13:23:13 -0300

    Update script.js
    Porta ajustada

--- SEMANA_06/03_AUT_EST_ENTREGA/script.js
+++ SEMANA_06/03_AUT_EST_ENTREGA/script.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hostname = '127.0.0.1';
 
 
-const port = 3052;
+const port = 3051;
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const DBPATH = 'dbUser.db';
